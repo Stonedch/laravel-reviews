4529231d9a9e6c35989ef65691de4549e2a938ef
stonedch <stch.max.muhin@gmail.com>
2020-08-20 21:10:38 +0300

    chore: update home page

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -22,9 +22,11 @@
                                             <p class="card-text mb-auto"> {{ $product->description }} </p>
                                             <a href="#" class="stretched-link"> Подробнее </a>
                                         </div>
-                                        <div class="col-auto d-none d-lg-block">
-                                            <svg class="bd-placeholder-img" width="200" height="200" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                                        </div>
+                                        @if ($product->picture)
+                                            <div class="col-auto d-none d-lg-block">
+                                                <svg class="bd-placeholder-img" width="200" height="200" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                                            </div>
+                                        @endif
                                     </div>
                                 </div>
                             @endforeach
