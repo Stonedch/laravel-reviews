24674c59d01c7d98a309368642d73e7e37beec51
stonedch <stch.max.muhin@gmail.com>
2020-08-31 16:10:21 +0300

    refactor: minor code style changes in Models

--- src/app/Models/Products.php
+++ src/app/Models/Products.php
@@ -4,9 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Products extends Model {
-    
+class Products extends Model
+{
     protected $table = 'products';
-    public $timestamps = false;
-
 }
--- src/app/Models/Reviews.php
+++ src/app/Models/Reviews.php
@@ -4,13 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Reviews extends Model {
-
-    public $table = 'reviews';
-    public $timestamps = false;
-
-    public function setCreatedAtAttribute($value) { 
-        $this->attributes['posted_at'] = \Carbon\Carbon::now(); 
-    }
-
+class Reviews extends Model
+{
+    protected $table = 'reviews';
 }
