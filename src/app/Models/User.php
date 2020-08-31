d7f8a2df9f85f6de701ecb45f955fe4d2e9c3601
stonedch <stch.max.muhin@gmail.com>
2020-08-31 23:49:52 +0300

    fix: get role of user

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
 
     public function role()
     {
-        return $this->belongsTo(Role::class);
+        return $this->belongsTo(Role::class, 'roles_id_foreign');
     }
 
     public function isAdmin()
